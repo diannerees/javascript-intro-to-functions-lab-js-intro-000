053edf35b8bfd87e10e9908ab2de61ca34def12e
Dianne Rees <drees3271@verizon.net>
2019-04-06 11:12:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function shout(string){return string.toUpperCase()};
+function whisper(string){return string.toLowerCase()};
+function logShout(string){console.log(string.toUpperCase())};
+function logWhisper(string){console.log(string.toLowerCase())};
+var string = "I love you, Grandma"
+function sayHiToGrandma(string){
+if (string === mixedCase){return "I love you, too"}}
