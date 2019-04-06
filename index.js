8b9a727eaeb51f57c055bc5dc2dde37d4c845294
Dianne Rees <drees3271@verizon.net>
2019-04-06 11:42:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function whisper(string){return string.toLowerCase()};
 function logShout(string){console.log(string.toUpperCase())};
 function logWhisper(string){console.log(string.toLowerCase())};
 var string = "I love you Grandma"
-function sayHiToGrandma () {
+function sayHiToGrandma (string) {
 if (string === lowercase)
 {
   return "I love you too"}
