7776a60a6516d99286d999d26a95c28c266d7dc0
Dianne Rees <drees3271@verizon.net>
2019-04-06 11:41:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ function shout(string){return string.toUpperCase()};
 function whisper(string){return string.toLowerCase()};
 function logShout(string){console.log(string.toUpperCase())};
 function logWhisper(string){console.log(string.toLowerCase())};
-var string = "I love you, Grandma"
-function sayHiToGrandma (string.toLowerCase("I love you , Grandma")) {
-if (string === lowercase){return "I love you, too"}}
+var string = "I love you Grandma"
+function sayHiToGrandma (string.toLowerCase("I love you Grandma")) {
+if (string === lowercase)
+{
+  return "I love you too"}
+}
